465d5a2ee8144c653f4677a8cc8d19546a5631ca
Lalit Haryani <lalit.haryani@nagarro.com>
2015-07-21 10:52:32 +0200

    removed file

--- app/assets/javascripts/angular_app/public/controllers/home/HomeCtrl.js
+++ app/assets/javascripts/angular_app/public/controllers/home/HomeCtrl.js
@@ -1,16 +1,20 @@
 var app = angular.module('InventoryManagement');
 app.controller('HomeCtrl', ['Auth', '$scope', '$window', function(Auth, $scope, $window){
 
-
- $scope.getCurrentUser = function(){
+  $scope.getCurrentUser = function(){
     Auth.currentUser().then(function(user) {
         
-      $scope.user = user
+       $scope.user = user
+
     }, function(error) {
         // unauthenticated error
-    });     
-};
+    });  
 
+  };
+
+ /* $scope.isUserAuthenticated = function(){
+    Auth.isAuthenticated();     
+  };*/
 
 
 }]);
